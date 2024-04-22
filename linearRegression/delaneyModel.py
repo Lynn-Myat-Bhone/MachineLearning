30be9b7ca564e8f4c87000728712b43be4895f4c
Lynn-Myat-Bhone <lynnmyatbhone311@gmail.com>
2024-04-22 15:27:06 +0630

    sklearnLinear

---
+++ linearRegression/delaneyModel.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
+
+#read data
+df  = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
+
+#define x and y
+y = df['logS']
+x = df.drop('logS',axis=1) 
+
+#split data to train and test
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size = 0.2,random_state=122)
+
+#Linear Regression
+model = LinearRegression()
+model.fit(x_train,y_train)
+
+#prediction
+y_train_pred = model.predict(x_train)
+y_test_pred = model.predict(x_test)
+
+
+# evaluate model performance
+# for training set
+model_train_mse = mean_squared_error(y_train,y_train_pred)
+model_train_r2 = r2_score(y_train,y_train_pred)
+
+# for testing set
+model_test_mse = mean_squared_error(y_test,y_test_pred)
+model_test_r2 = r2_score(y_test,y_test_pred)
+
+table = pd.DataFrame(["LinearRegression", model_test_mse, model_test_r2, model_train_mse, model_train_r2]).transpose()
+table.columns=["Method","MSE(Train)","MSE(Test)","R2(Train)","R2(Test)"]
+print(table)
+
+# Plot the CSV data and the linear regression line
+line = np.polyfit(y_train,y_train_pred,1)
+p = np.poly1d(line)
+plt.scatter (y_train,y_train_pred,c='green',alpha = 0.5)# alpha ajust transparent of data point
+plt.plot(y_train, p(y_train), color = 'red')  # Plotting the regression line
+plt.ylabel('Predict logS')
+plt.xlabel('logS')
+plt.show()
