e7f03c451ca0184c1b0728786abf84a162d035ed
Lynn-Myat-Bhone <lynnmyatbhone311@gmail.com>
2024-08-06 21:08:32 +0630

    update

---
+++ Handle_Data/XGBoost.py
@@ -0,0 +1,8 @@
+from xgboost import XGBRegressor
+
+my_model = XGBRegressor()
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
+my_model.fit(X_train, y_train, 
+             early_stopping_rounds=5, 
+             eval_set=[(X_valid, y_valid)], 
+             verbose=False)
