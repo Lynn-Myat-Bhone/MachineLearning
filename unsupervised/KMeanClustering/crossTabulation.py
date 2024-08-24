cf08e783fa2c94c0c1cb1a64ba6a7aaaa8d38536
Lynn-Myat-Bhone <lynnmyatbhone311@gmail.com>
2024-08-24 20:26:53 +0630

    updates

---
+++ unsupervised/KMeanClustering/crossTabulation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#this is just Example of how to input Cross-Tabulation.
+
+#labels is  predicted values from model and varieties giving the grain variety for each sample.
+# Create a DataFrame with labels and varieties as columns: df
+df = pd.DataFrame({'labels': labels, 'varieties': varieties})
+
+# Create crosstab: ct
+ct = pd.crosstab(df['labels'],df['varieties'])
+
+# Display ct
+print(ct)
