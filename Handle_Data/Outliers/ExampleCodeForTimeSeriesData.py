afc12035e31ba16d73f5a3a4d8963ba5a9f440d4
Lynn-Myat-Bhone <lynnmyatbhone311@gmail.com>
2024-12-07 11:13:42 +0630

    update

---
+++ Handle_Data/Outliers/ExampleCodeForTimeSeriesData.py
@@ -0,0 +1,15 @@
+def replace_outliers(series):
+    # Calculate the absolute difference of each timepoint from the series mean
+    absolute_differences_from_mean = np.abs(series - np.mean(series))
+    
+    # Calculate a mask for the differences that are > 3 standard deviations from zero
+    this_mask = absolute_differences_from_mean > (np.std(series) * 3)
+    
+    # Replace these values with the median accross the data
+    series[this_mask] = np.nanmedian(series)
+    return series
+
+# Apply your preprocessing function to the timeseries and plot the results
+prices_perc = prices_perc.apply(replace_outliers)
+prices_perc.loc["2014":"2015"].plot()
+plt.show()
---
+++ Handle_Data/Outliers/Readme.md
@@ -0,0 +1,2 @@
+<h3>Outlier</h3>
+<p>Outliers are data points that are different from data set(usually away from 3 std)</p>
